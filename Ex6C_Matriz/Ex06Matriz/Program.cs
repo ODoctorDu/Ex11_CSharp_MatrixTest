4d03218bcabacadf8b53328946826f2b3ef51c16
ODoctorDu <odoctordu@gmail.com>
2021-03-12 14:52:20 -0300

    First Matrix Exercise

---
+++ Ex6C_Matriz/Ex06Matriz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ex06Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is a Square Matrix. Please, enter a number for the Rows and Lines: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the numbers of the #" + (i+1) + " Line");
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++ )
+                {
+                    matriz[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("\n--------------------\nMain Diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+            Console.WriteLine("\n--------------------\nSearching for Negative Numbers...");
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i,j] < 0)
+                    {
+                        sum++;
+                    }
+                }
+            }
+            Console.WriteLine("There is " + sum + " negative numbers in the array");
+        }
+    }
+}
